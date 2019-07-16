439894892ee62f64bbd23bad1638639f885877e7
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-16 19:33:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+  def initialize
+    @word = word
+  end
+
+  def match(w)
+    (@word.split("").sort == w.split("").sort)
+   end
+  end
+
+  end
+  # listen = Anagram.new("listen")
+  # listen.match(%w(enlists google inlets banana))
